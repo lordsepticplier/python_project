d47911bc5d8c64b6d7de1dc8a59c0967399390a4
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2023-03-17 15:42:42 +1300

    Add files via upload
    I have now done some of the if practices today and most of the elif practices also there were no comments on the last commits because I was testing that GitHub would record it correctly now that I know it does I will keep this updated with the changes and why

---
+++ if_prac.py
@@ -0,0 +1,25 @@
+#col=input("put a letter of a chess board cord")
+#row=int(input("now put the number for the cord"))
+#if col == "a" or "c" or "e" or "g": 
+#    if row%2==0:
+#        print("the squre is white")
+#    else:
+#        print("the squre is black")
+#else:
+#    if row%2==0:
+#        print("the squre is black")
+#    else:
+#        print("the squre is white")
+
+
+#side1=int(input("put the first side"))
+#side2=int(input("put the second side"))
+#side3=int(input("put the last side"))
+#if side1==side2 and side2==side3: 
+#    print("equilateral triangle")
+#elif side1==side2 or side2==side3 or side1==side3:
+#    print("isosceles triangle")
+#else:
+#    print("scalene triangle")
+
+
--- year.py
+++ year.py
@@ -3,10 +3,11 @@ zodiac = ['dragon','snake','horse','sheep','monkey','rooster','dog','pig','rat',
 year1 = (year1-2000)%12
 print(year1,"is the year of the ",zodiac[year1])
 
+
 year = int(input("year"))
-#print(year)
+print(year)
 year = (year-2000)%12
-#print(year)
+print(year)
 if year==0:
     print(year,"is the year of the dragon")
 elif year==1:
@@ -34,3 +35,68 @@ elif year==11:
 else:
     print("is this within the parapters?")
 
+
+mag = float(input("magnitude"))
+#print(mag)
+if mag<2.0:
+    print(mag,"is micro")
+elif mag<3.0:
+    print(mag,"is very minor")
+elif mag<4.0:
+    print(mag,"is minor")
+elif mag<5.0:
+    print(mag,"is light")
+elif mag<6.0:
+    print(mag,"is moderate")
+elif mag<7.0:
+    print(mag,"is strong")
+elif mag<8.0:
+    print(mag,"is major")
+elif mag<10.0:
+    print(mag,"is great")
+elif mag>=10.0:
+    print(mag,"is meteoric")
+else:
+    print("is this within the parapters?")
+
+
+sou = int(input("sound level"))
+#print(sou)
+if sou>130:
+    print(sou,"is loader then even the mighty jackhammer")
+elif sou==130:
+    print("jackhammer")
+elif sou>106:
+    print(sou,"is between jackhammer and petrol lawnmower")
+elif sou==106:
+    print("petrol lawnmower")
+elif sou>70:
+    print(sou,"is between petrol lawnmower and alarm clock")
+elif sou==70:
+    print("alarm clock")
+elif sou>40:
+    print(sou,"is between alarm clock and quite room")
+elif sou==40:
+    print("quite room")
+elif sou<40:
+    print(sou,"is quiter then even the sly quite room")
+else:
+    print("is this within the parapters?")
+
+
+voco = input("letter")
+#print(voco)
+if voco=="a":
+    print(voco,"is vowel")
+elif voco=="e":
+    print(voco,"is vowel")
+elif voco=="i":
+    print(voco,"is vowel")
+elif voco=="o":
+    print(voco,"is vowel")
+elif voco=="u":
+    print(voco,"is vowel")
+elif voco=="y":
+    print(voco,"is vowel sometimes and other times a consonant")
+else:
+    print(voco,"is a consonant")
