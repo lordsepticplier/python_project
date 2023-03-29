90e15326ddc449d9ba22a942c9d54344e7e925d6
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2023-03-29 16:33:50 +1300

    Add files via upload
    I added some functions in the firstclass file in the Ticket class and am nearly done with all the things I will need to add to the ticket class and will be moving on to inputs and the use of the ticket class.

--- firstclass.py
+++ firstclass.py
@@ -1,24 +1,43 @@
 class Ticket:
-    def __init__(self,staffID,Tname,contact,desc):
-        self.staffID=staffID
-        self.Tname=Tname
-        self.contact=contact
+    TID=2000
+    Tresponse="Not Yet Provided"
+    Tstatus="Open"
+    TList=[]
+    def __init__(self,SID,Tcreator,Email,desc):
+        Ticket.TID+=1
+        self.SID=SID
+        self.Tcreator=Tcreator
+        self.Email=Email
         self.desc=desc
-    def getstaffID(self):
-        return self.staffID
-    def setstaffid(self,staffID):
-        self.staffID=staffID
-    def getTname(self):
-        return self.Tname
-    def setTname(self,Tname):
-        self.Tname=Tname   
-    def getcontact(self):
-        return self.contact
-    def setcontact(self,contact):
-        self.contact=contact
+        Ticket.Tlist.append(self)
+    def getSID(self):
+        return self.SID
+    def setSID(self,SID):
+        self.SID=SID
+    def getTcreator(self):
+        return self.Tcreator
+    def setTcreator(self,Tcreator):
+        self.Tcreator=Tcreator   
+    def getEmail(self):
+        return self.Email
+    def setEmail(self,Email):
+        self.Email=Email
     def getdesc(self):
         return self.desc
     def setdesc(self,desc):
         self.desc=desc 
-
+    def getTresponse(self):
+        return self.Tresponse
+    def setTresponse(self,Tresponse):
+        self.Tresponse=Tresponse
+    def getTstatus(self):
+        return self.Tstatus
+    def setTstatus(self,Tstatus):
+        #COOOOOOOOOOOOOOODE THE IF STATEMENT HERE PLEASE
+        self.Tstatus=Tstatus
+    def changePassword(self):
+        if self.desc.lower()=="password change":
+            password="New password generated: "+self.SID[0:2]+self.Tcreator[0:3]
+            self.setTresponse(password)
+            self.setTstatus("Closed")
 tkt=Ticket()
