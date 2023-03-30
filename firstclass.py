af8dd0a0fa4a0ea2b100c5b51cf1bd421bf17153
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2023-03-30 16:14:28 +1300

    class functions
    I have done most of the functions in the ticket class only about two left to go and then I am going onto using the class

--- firstclass.py
+++ firstclass.py
@@ -1,10 +1,15 @@
 class Ticket:
     TID=2000
+    Tcreated=0
+    Topen=0
+    Tresolved=0
     Tresponse="Not Yet Provided"
     Tstatus="Open"
     TList=[]
     def __init__(self,SID,Tcreator,Email,desc):
         Ticket.TID+=1
+        Ticket.Tcreated+=1
+        Ticket.Topen+=1
         self.SID=SID
         self.Tcreator=Tcreator
         self.Email=Email
@@ -30,14 +35,28 @@ class Ticket:
         return self.Tresponse
     def setTresponse(self,Tresponse):
         self.Tresponse=Tresponse
+        self.setTstatus("Closed")
     def getTstatus(self):
         return self.Tstatus
     def setTstatus(self,Tstatus):
-        #CODE THE IF STATEMENT HERE PLEASE
         self.Tstatus=Tstatus
+        if self.Tstatus.lower()=="closed":
+            Ticket.Topen-=1
+            Ticket.Tresolved+=1
+        elif self.Tstatus.lower()=="reopened":
+            Ticket.Topen+=1
+            Ticket.Tresolved-=1
+        else:
+            print("something very wrong is happening here")
     def changePassword(self):
         if self.desc.lower()=="password change":
             password="New password generated: "+self.SID[0:2]+self.Tcreator[0:3]
             self.setTresponse(password)
             self.setTstatus("Closed")
+    def resolve(self):
+        self.setTstatus("Closed")
+    def reopen(self):
+        self.setTstatus("Reopened")
+
+
 tkt=Ticket()
