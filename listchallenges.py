d544bfe50ed13631190f3b8caefadb4a5401f0bd
lordsepticplier <126767712+lordsepticplier@users.noreply.github.com>
2023-04-05 16:32:01 +1200

    done more challenges
    I have done another one of the challenges

--- listchallenges.py
+++ listchallenges.py
@@ -11,3 +11,15 @@ print(connection([5,3,1],[4,6,5,8]))
 print(connection([2,3,1],[4,6,5,8]))
 #used to test that the fuction works being that it returns true if the lists have one common member
 
+def minOfList(list1,n):
+    result=[]
+    for word in list1:
+       if len(word)>n:
+           result.append(word)
+    return result
+             
+print(minOfList(["harry","pet","peter","e"],4))
+print(minOfList(["harry","pet","peter","e"],2))
+print(minOfList(["happy","pet","peter","e"],7))
+print(minOfList(["harry","pet","peter","eeeeeee"],6))
+#used to test that the fuction works being that it returns all the words in a list that are longer then n
